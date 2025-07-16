7b03ab7c538948b5afe5fa6ffef0c947cef01410
Peter C. Romano <services@vitruvian.tech>
2025-07-16 11:37:03 -0400

    Update hitbox.ts

--- directives/hitbox.ts
+++ directives/hitbox.ts
@@ -2,9 +2,9 @@
  * Copyright (c) 2025 Vestive, LLC
  *
  * This source code is licensed under the MIT license.
- * See https://github.com/vestive/nuxt-hitbox/blob/main/LICENSE for details.
+ * See https://github.com/Get-Vestive/nuxt-hitbox/blob/main/LICENSE for details.
  */
-import { useFloatingNumber } from '@/composables/useHitbox'
+import { useHitbox } from '@/composables/useHitbox'
 
 export default {
   mounted(el: HTMLElement, binding: any) {
