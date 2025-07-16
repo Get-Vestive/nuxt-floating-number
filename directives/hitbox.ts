00655ced6923cb43a2a56255b9752fd8c4fba7f8
Peter C. Romano <services@vitruvian.tech>
2025-07-16 11:25:31 -0400

    Create hitbox.ts

---
+++ directives/hitbox.ts
@@ -0,0 +1,17 @@
+/*!
+ * Copyright (c) 2025 Vestive, LLC
+ *
+ * This source code is licensed under the MIT license.
+ * See https://github.com/vestive/nuxt-hitbox/blob/main/LICENSE for details.
+ */
+import { useFloatingNumber } from '@/composables/useHitbox'
+
+export default {
+  mounted(el: HTMLElement, binding: any) {
+    const { trigger } = useFloatingNumber()
+    el.addEventListener('pointerdown', () => {
+      const { quantity = 1, position = 'above' } = binding.value || {}
+      trigger(el, quantity, position)
+    })
+  }
+}
